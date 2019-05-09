470416a9cf3379ba7c71b6602890a60cd4b9b869
zhengbo <zhengb@jiagouyun.com>
2019-05-09 16:00:18 +0800

    fix arrow function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = function (method) {
   var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
-  var depsStr =  method.toString().match(FN_ARGS);
+  var Arrow_ARGS = /^\s*[^\(]*\(\s*([^\)]*)\)\s*=>/m;
+  var depsStr =  method.toString().match(FN_ARGS) || method.toString().match(Arrow_ARGS)
   var deps = (depsStr) ? depsStr[1].trim().split(/[\s,]+/) : [''];
   
   if(deps[0] === '') return [];
